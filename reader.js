9f19e9d2f317e48da9635fa5ba37f216a08d5b0d
Lucian Condrea <luciancondreadev@gmail.com>
2019-01-10 13:16:36 -0500

    Add footer

--- reader.js
+++ reader.js
@@ -12,6 +12,7 @@
     document.addEventListener("DOMContentLoaded", function() {
         addButton("navigate-previous");
         addButton("navigate-next");
+        document.getElementById("top_chapter_list").classList.remove("chapter_select");
     })
 
     function addButton(id) {
@@ -23,11 +24,15 @@
 
     function toggleMenu(e) {
         console.log("Ran function");
+        
         if (e.key === "Escape") {
             let header = document.getElementsByTagName("header")[0];
             let plug = document.getElementsByClassName("plug-right")[0];
+            let footer = document.getElementsByClassName("manga_read")[0];
             header.style.height = header.style.height === "" ? "81px" : "";
             plug.style.width = plug.style.width === "" ? "30px" : "";
+            footer.style.height = footer.style.height === "" ? "160px" : "";
+            footer.style.borderTopWidth = footer.style.borderTopWidth === "" ? "10px" : "";
         }
     }
 
