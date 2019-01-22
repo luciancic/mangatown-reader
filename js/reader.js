9fd4dd6edd9db7cab10992089df75c92f09e333e
Lucian Condrea <luciancondreadev@gmail.com>
2019-01-22 15:47:11 -0500

    Make pointer and menu revealable before document is fully loaded

--- js/reader.js
+++ js/reader.js
@@ -1,21 +1,20 @@
 (function IIFE(global, lib) {
     
-    document.onreadystatechange = function() {
-        if (document.readyState === "complete") {
-            document.getElementById("navigate-previous").onclick = global.wrappedJSObject.previous_page;
-            document.getElementById("navigate-next").onclick = global.wrappedJSObject.next_page;
-            document.onkeydown = lib.toggleMenu;
-            document.onmousemove = lib.makeCountdown(); // This function returns a function with a closure, this is not a typo.
-        }
-    }
-    
     // Load here so that it takes effect immediately
     document.addEventListener("DOMContentLoaded", function() {
         lib.addButton("navigate-previous");
         lib.addButton("navigate-next");
         lib.hideCursor();
         document.getElementById("top_chapter_list").classList.remove("chapter_select");
+        document.onkeydown = lib.toggleMenu;
+        document.onmousemove = lib.makeCountdown(); // makeCountdown returns a function with a closure, this is not a typo.
     })
-
-
+    
+    document.onreadystatechange = function() {
+        if (document.readyState === "complete") {
+            document.getElementById("navigate-previous").onclick = global.wrappedJSObject.previous_page;
+            document.getElementById("navigate-next").onclick = global.wrappedJSObject.next_page;
+        }
+    }
+    
 })(window, window.MangatownReaderLib)
